a7bc11caa4dc45e010070d797c8d2031183abaeb
GaluhManafi <48429817+GaluhManafi@users.noreply.github.com>
2019-04-24 15:44:35 +0700

    Update db.php

--- app-cordova/www/db.php
+++ app-cordova/www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8941204_ruth","111222","id8941204_kontak") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id8941173_galuh","12345","id8941173_kontak") or die ("could not connect database");
 ?>
